96ba01017e3ee35b6d163d7d49d12107732c2d42
Francis ABODO <abodojustin@gmail.com>
2023-02-27 19:11:46 +0100

    update project architecture

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'opensans': ['Open Sans', 'sans-serif']
+      },
+      screens: {
+        'lg': '1100px',
+      },
+    },
+    colors: {
+      ...colors,
+      primary: {
+        dark: '#213665',
+        light: '#3B5A9F'
+      },
+      white: '#FFFFFF',
+      black: '#000000'
+    },
   },
   plugins: [],
 };
