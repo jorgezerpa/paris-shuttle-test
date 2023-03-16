6140eeab0f1fc97b37f23989a6b0d84d21dd68fa
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-03-16 10:25:29 -0400

    add loading while navigating

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,5 @@
 import { component$, Slot, useSignal } from '@builder.io/qwik';
+import { useLocation } from '@builder.io/qwik-city';
 import Header from '~/components/header/header';
 import Footer from '~/components/footer/footer';
 
@@ -6,16 +7,25 @@ import { DrawerMobile } from '~/libs/drawerMobile/drawerMobile';
 
 
 export default component$(() => {
+  const { isNavigating } = useLocation();
   const showMobileMenu = useSignal(false)
 
   return (
     <div class='overflow-x-clip'>
-      <main >
-        <Header showNavbar={showMobileMenu}/>
-        <section>
-          <Slot />
-        </section>
-      </main>
+      <Header showNavbar={showMobileMenu}/>
+      { isNavigating && (
+        <div class={'text-xl text-black min-h-screen justify-center items-center flex'}>
+          <div class="lds-facebook"><div></div><div></div><div></div></div>
+        </div>
+
+      )}
+      { !isNavigating &&  (
+        <main >
+          <section>
+            <Slot />
+          </section>
+        </main>
+      )}
       <Footer/>
       <DrawerMobile showNavbar={showMobileMenu} />
     </div>
