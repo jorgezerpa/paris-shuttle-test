73d97129866baad19caf3eeaa140aff503b9ea1c
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-03-17 10:35:43 -0400

    add languages logic to home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,34 @@ import { globalContext } from '~/store/context/mainContext';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { BannerForm } from '~/components/shared/bannerForm/bannerForm';
 
+interface HomeLanguageObject {
+  "title":"TRANSFERTS ENTRE LES AÉROPORTS, PARIS ET DINEYLAND.",
+  "description":"Notre site vous propose des solutions de transfert (navette privée, voiture avec chauffeur privé, navette partagée, taxi sur mesure) entre les différents aéroports de Paris, Charles de gaulle, Orly et Beauvais afin d'eviter les attentes aux bus, trains et autres taxi onéreux.",
+  "packsTitle":"NOS PACK FAMILY DISNEYLAND.",
+  "privateCarTitle":"VOITURE PRIVÉE",
+  "privateCarItems":["Service Chauffeur","Départ immédiat","Voyagez seul à bord","Service de porte à porte","Chauffeur Professionnel"],
+  "privateVANTitle":"VAN Privé",
+  "privateVANItems":["Service Confortable","Service porte à porte","Siège bébé","Idéal pour les groupes","Navette de 8 personnes"]
+}
+export const HomeLanguageObjectDefault:HomeLanguageObject =  {
+  "title":"TRANSFERTS ENTRE LES AÉROPORTS, PARIS ET DINEYLAND.",
+  "description":"Notre site vous propose des solutions de transfert (navette privée, voiture avec chauffeur privé, navette partagée, taxi sur mesure) entre les différents aéroports de Paris, Charles de gaulle, Orly et Beauvais afin d'eviter les attentes aux bus, trains et autres taxi onéreux.",
+  "packsTitle":"NOS PACK FAMILY DISNEYLAND.",
+  "privateCarTitle":"VOITURE PRIVÉE",
+  "privateCarItems":["Service Chauffeur","Départ immédiat","Voyagez seul à bord","Service de porte à porte","Chauffeur Professionnel"],
+  "privateVANTitle":"VAN Privé",
+  "privateVANItems":["Service Confortable","Service porte à porte","Siège bébé","Idéal pour les groupes","Navette de 8 personnes"]
+}
+
 export default component$(() => {
   const context = useContext(globalContext)
-  const languageData = useSignal<any>({test:'fr'}) //here is all the json dta for each lang
+  const languageData = useSignal<HomeLanguageObject>(HomeLanguageObjectDefault) //here is all the json dta for each lang
 
   useTask$(({track})=>{
     track(()=>context.language) 
     fetch(`http://localhost:5173/translations/${context.language}.json`)
       .then(data => data.json())
-      .then(data => languageData.value = data)
+      .then(data => languageData.value = data.home as HomeLanguageObject)
       .catch(e=>console.log(e))
   })
 
@@ -24,9 +43,9 @@ export default component$(() => {
         <div class="w-full py-10 flex items-center justify-center gap-10 lg:flex-row flex-col">
           <div class='w-full'>
             <div class="border-b border-b-primary-dark mb-5">
-              <h2 class="lg:text-2xl text-xl text-primary-dark font-bold">TRANSFERTS ENTRE LES AÉROPORTS, PARIS ET DINEYLAND.</h2>
+              <h2 class="lg:text-2xl text-xl text-primary-dark font-bold">{languageData.value.title}</h2>
             </div>
-            <p class="text-gray-800 ">Notre site vous propose des solutions de transfert (navette privée, voiture avec chauffeur privé, navette partagée, taxi sur mesure) entre les différents aéroports de Paris, Charles de gaulle, Orly et Beauvais afin d'eviter les attentes aux bus, trains et autres taxi onéreux.</p>
+            <p class="text-gray-800 ">{ languageData.value.description }</p>
           </div>
           <div class='w-full flex justify-center items-center'>
             <img class="min-w-[280px]" src="/images/photo_about.png" alt="" />
@@ -38,7 +57,7 @@ export default component$(() => {
 
         <div class='py-1 w-full  max-w-6xl mx-auto'>
         <div class="border-b border-b-primary-dark mb-5 mt-10 md:mx-2 mx-0">
-          <h2 class="lg:text-2xl text-2xl text-white md:text-primary-dark font-bold">NOS PACK FAMILY DISNEYLAND.</h2>
+          <h2 class="lg:text-2xl text-2xl text-white md:text-primary-dark font-bold">{ languageData.value.packsTitle }</h2>
         </div>
         <div class="flex justify-center pb-10">
           <div class="flex flex-col lg:flex-row  items-center justify-center flex-wrap lg:flex-nowrap px-1  gap-5 w-full max-w-[1300px]  ">
@@ -79,21 +98,17 @@ export default component$(() => {
         <div class="flex justify-center pb-10">
           <div class="flex flex-col lg:flex-row  items-center justify-center flex-wrap lg:flex-nowrap px-1  gap-5 w-full max-w-6xl  ">
             <div class=" p-5 shadow-sm shadow-[rgba(60,60,60,.5)] bg-white w-full bg-center bg-no-repeat bg-cover" style={{ backgroundImage: 'url(/images/background_navette_privee.png)' }} >
-              <h2 class="mb-2 text-xl text-primary-dark font-bold">VOITURE PRIVÉE</h2>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Service Chauffeur</p>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Départ immédiat</p>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Voyagez seul à bord</p>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Service de porte à porte</p>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Chauffeur Professionnel</p>
+              <h2 class="mb-2 text-xl text-primary-dark font-bold">{ languageData.value.privateCarTitle }</h2>
+              { languageData.value.privateCarItems.map((item, index)=>(
+                  <p key={'privateCarHomeItem'+index} class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span>{ item }</p>
+              )) }
             </div>
 
             <div class=" p-5 shadow-sm shadow-[rgba(60,60,60,.5)] bg-white w-full bg-center bg-no-repeat bg-cover" style={{ backgroundImage: 'url(/images/background_navette_partagee.png)' }} >
-              <h2 class="mb-2 text-xl text-primary-dark font-bold">VAN Privé</h2>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Service Confortable</p>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Service porte à porte</p>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Siège bébé</p>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Idéal pour les groupes</p>
-              <p class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span> Navette de 8 personnes</p>
+              <h2 class="mb-2 text-xl text-primary-dark font-bold">{ languageData.value.privateVANTitle }</h2>
+              { languageData.value.privateVANItems.map((item, index)=>(
+                  <p key={'privateVANHomeItem'+index} class="mb-1 text-gray-600 flex items-center gap-2"><span><img src="/images/plus_list.png" alt="" /></span>{ item }</p>
+              )) }
             </div>
 
           </div>
